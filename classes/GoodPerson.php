de50cc526a3ac419fc84dc772e61717ded378fa9
danish981 <danishmehmood981@gmail.com>
2021-10-01 04:04:37 +0500

    added a function goodPerson.php class in classes/

--- classes/GoodPerson.php
+++ classes/GoodPerson.php
@@ -2,10 +2,18 @@
 
 class GoodPerson {
     protected $nextLine = "\r\n";
+    private string $personName = "danish mehmood";
 
     public function getMyName(): string {
         return "danish mehmood mughal " . $this->nextLine;
     }
+
+    public function singlePerson() : string {
+        return "the single person " . $this->personName;
+    }
+
+
+
 }
 
 
